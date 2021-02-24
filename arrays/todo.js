c5022d40fa1ee3b72723dfa49e488591f434edef
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-02-24 15:50:59 -0600

    Add examples about use loop and callbacks in array

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,10 +1,11 @@
 const toDo = ['wash the dishes', 'clean the house', 'walk the dog', 'wash clothes', 'do excercise']
 
-//console.log(`You have ${toDo.length} to do`)
-//console.log(`To do: ${toDo[0]}`)
-//console.log(`To do: ${toDo[toDo.length - 2]}`)
-
 toDo.splice(2, 1)
 toDo.push('Clean')
 toDo.shift()
-console.log(toDo)
+
+console.log(`You have ${toDo.length} to do!`)
+
+toDo.forEach(function(todo, index){
+    console.log(`${index + 1}. ${todo}`)
+})
