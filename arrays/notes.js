01d529b1e551efc802e270d9a21fe4416e2c8c17
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-02-24 19:45:04 -0600

    Add files via upload

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,29 +1,35 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Excercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
-console.log(notes.pop())
-notes.push('My new item')
+//console.log(notes.pop())
+//notes.push('My new item')
 
-console.log(notes.shift())
-notes.unshift('My first note')
+//console.log(notes.shift())
+//notes.unshift('My first note')
 
 //start at, remove this, rewrite this
-notes.splice(1, 1, 'This is the second new item')
+//notes.splice(1, 1, 'This is the second new item')
 
-notes[2] = 'This is the new third item'
+//notes[2] = 'This is the new third item'
 
-
-notes.forEach(function(item, index){
-    console.log(index)
-    console.log(item)
-})
+//notes.forEach(function(item, index){
+//    console.log(index)
+//    console.log(item)
+//})
 
 console.log(notes.length);
 console.log(notes)
 
-for (let count = 0 ; count <= 2 ; count++) {
-    console.log(count);
-}
-
-for (let count = notes.length - 1 ; count >= 0 ; count --) {
-    console.log(notes[count])
-}
+const index = notes.findIndex(function(note, index){
+    console.log(note)
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
