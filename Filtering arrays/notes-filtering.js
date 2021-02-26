7f49ae4307ec5319b81127c30607e0c43a5f21e7
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-02-26 10:42:55 -0600

    Add an example using filter in object arrays

---
+++ Filtering arrays/notes-filtering.js
@@ -0,0 +1,45 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Excercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index){
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+const findNotes =  function(notes, query){
+    return notes.filter(function (notes, noteTitle){
+        const isTitleMatch = notes.title.toLowerCase().includes(query.toLowerCase())
+        const isBodyMatch = notes.body.toLowerCase().includes(query.toLowerCase())
+        return isTitleMatch || isBodyMatch
+    })
+}
+
+console.log(findNotes(notes, 'to'))
+
+// const findNote = function (note, noteTitle) {
+//     const index = notes.findIndex(function (note, index){
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+const note = findNote(notes, 'some other office modification')
+console.log(note)
+
+// console.log(notes.length);
+// console.log(notes)
+
+// const index = notes.findIndex(function(note, index){
+//     console.log(note)
+//     return note.title === 'Habbits to work on'
+// })
+// console.log(index)
