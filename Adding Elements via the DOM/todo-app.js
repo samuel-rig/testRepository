68dfa55fda5786c38a139b6afd055d464ceeb51f
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-03-01 02:20:33 -0600

    Add elements dinamiclly in pages using DOM

---
+++ Adding Elements via the DOM/todo-app.js
@@ -0,0 +1,31 @@
+const todos = [{
+    text: 'wash the dishes',
+    completed: false
+}, {
+    text: 'clean the house',
+    completed: false
+}, {
+    text:'walk the dog',
+    completed: true
+}, {
+    text: 'wash clothes',
+    completed: false
+}, {
+    text: 'do excercise',
+    completed: true
+}]
+
+const incomplete = todos.filter(function(to){
+    return !to.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incomplete.length} things to do left!`
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach(function(to){
+        const todo = document.createElement('p')
+        todo.textContent = to.text
+        document.querySelector('body').appendChild(todo)
+})
