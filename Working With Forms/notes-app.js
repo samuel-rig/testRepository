683bddaa0234961fb7bd6d1a1e850c33d9f9e96e
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-03-02 01:55:50 -0600

    Use forms to add items to an object array with JS

---
+++ Working With Forms/notes-app.js
@@ -0,0 +1,46 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Excercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters){
+    const filterNotes = notes.filter(function (note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filterNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function(e){
+    console.log('It works')
+    e.target.textContent = 'The buton was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#name-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target.elements.firstName.value)
+    e.target.elements.firstName.value = ''
+})
