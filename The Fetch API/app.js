8ee07cba034417d5713d7c94c749d119bff2385b
samuel-rig <79431744+samuel-rig@users.noreply.github.com>
2021-03-15 17:42:45 -0600

    Setting up the fetch API to see how it works

---
+++ The Fetch API/app.js
@@ -0,0 +1,37 @@
+const puzzleEl = document.querySelector('#puzzle')
+const guessEl = document.querySelector('#guesses')
+const word1 = new Hangman('Cars 2', 2)
+
+puzzleEl.textContent = word1.puzzle
+guessEl.textContent = word1.statusMessage
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    word1.setLetters(guess)
+    puzzleEl.textContent = word1.puzzle
+    guessEl.textContent = word1.statusMessage
+})
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+// getCountry('MX').then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+    if (response.status === 200) {
+        return response.json()
+    } else {
+        throw new Error('Uneable to fetch the puzzle')
+    }
+}).then((data) => {
+    console.log(data.puzzle)
+}).catch((error) => {
+    console.log(error)
+})
